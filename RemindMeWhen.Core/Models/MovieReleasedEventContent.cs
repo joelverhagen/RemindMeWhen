bce7b8f8db45df98116a10aa7f578e0dc654945c
Joel Verhagen <joel.verhagen@gmail.com>
2014-11-02 12:47:51 -0800

    Add missing model

---
+++ RemindMeWhen.Core/Models/MovieReleasedEventContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Knapcode.RemindMeWhen.Core.Models
+{
+    public class MovieReleasedEventContent
+    {
+        public string Title { get; set; }
+        public int? ReleasedYear { get; set; }
+        public Uri ImdbUrl { get; set; }
+        public Uri RottenTomatoesUrl { get; set; }
+        public Uri ImageUrl { get; set; }
+        public IEnumerable<string> Actors { get; set; }
+    }
+}
